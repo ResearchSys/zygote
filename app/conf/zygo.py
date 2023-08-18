6f84ab55eed703cae05f4e2fe38b14e5c3250e7c
rexionmars <opensource.leonardi@gmail.com>
2023-08-18 15:52:59 -0300

    feat: create zygo config

---
+++ app/conf/zygo.py
@@ -0,0 +1,24 @@
+# Tamanho real da folha de redação do enem
+IMG_WIDTH = 2480
+IMG_HEIGHT = 3071
+
+# Cor da folha
+BACKGROUND_IMG = (255, 255, 255) # Tupla RGB
+
+# Dataset config
+CSV_PATH = "../data/uuid_essay-br.csv"
+
+# Configurações das fontes
+FONT_LIST = [
+    "../fonts/ttf/Glam_Queen.ttf",
+    "../fonts/ttf/Borel-Regular.ttf",
+    "../fonts/ttf/LeagueScript.ttf",
+    "../fonts/ttf/Thesignature.ttf",
+]
+
+TITLE_FONT_SIZE = 100
+BODY_FONT_SIZE = 100
+FONT_SIZE = 100
+
+# Saida das imagens criadas
+IMAGE_DIR = "imgs"
